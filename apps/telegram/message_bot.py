3e776baaef636e4a092cee5565b8b33853d4c01e
Mokhlesur Mahin <mokhlesur.rahman@pathao.com>
2023-03-27 22:12:20 +0600

    telegram api updated

--- apps/telegram/message_bot.py
+++ apps/telegram/message_bot.py
@@ -1,20 +1,22 @@
-import telegram
-import asyncio
+import requests
 
-async def send_telegram_message():
-    bot = telegram.Bot(token='6111218899:AAE-K0QPGJ1LruE_6fPynLpNqGsnzBP2rSE')
-    await bot.send_message(chat_id='2036238925', text={
-        "name":"mahin",
-        "contact": "",
-    })
+url = "https://api.telegram.org/bot6111218899%3AAAE-K0QPGJ1LruE_6fPynLpNqGsnzBP2rSE/sendMessage"
 
+payload = {
+    "text": {"data": "mahin"},
+    "disable_web_page_preview": False,
+    "disable_notification": False,
+    "reply_to_message_id": None,
+    "chat_id": "2036238925"
+}
+headers = {
+    "accept": "application/json",
+    "content-type": "application/json"
+}
 
-# define a function to call the async function
-def main():
-    asyncio.run(send_telegram_message())
+response = requests.post(url, json=payload, headers=headers)
 
-# call the main function to run the async function
-main()
+print(response.text)
 
 
 # get chat if from "https://telegram-bot-sdk.readme.io/reference/getme" getUpdate
